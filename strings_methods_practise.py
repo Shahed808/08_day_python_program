24a52cbf5b68cd08452b13df337d91743acf5984
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-06-20 11:06:14 +0530

    Add files via upload

---
+++ strings_methods_practise.py
@@ -0,0 +1,43 @@
+''' Practise session'''
+
+# Diff b/w index, count, find
+
+# s = ('python' 'programming')        # 2
+# print(s.count('m'))
+
+# d = ('Wecome! to my world')   # 3
+# print(d.index('o'))
+# print(d.rindex('o'))        # 15  
+
+# c = ('Hello! Welcome to coding environment')
+# f = c.find('e')
+# l = c.rfind('e')
+# print(f)
+# print(l)
+
+
+
+# o = ('Shahed')
+# print(o[-4])
+
+# s = "I,want,to,be,a,programmer"
+# d = s.split(',')
+# print(d)
+
+# b = ' ....ldjfhhf//..,,skdk,  Biryani  mmjd..,,skmd           '
+# c = b.strip(" ..,ldjksmhf/ ")
+# print(c+','+" is my favorite food. ")
+
+# r = b.rstrip(" ..,ldjksmhf/ ")
+# print(r)
+# print(b.lstrip(" ..,ldjksmhf/ "))
+
+# e = "I am a developer\nWelcome! to my world"
+# d =e.splitlines(False)
+# print(d)
+
+
+f = input()
+print(type(f))
+
+
